dfd6848798869a7546e927e955682db15626ab63
Shantanu391 <53044751+Shantanu391@users.noreply.github.com>
2021-07-02 09:47:32 +0530

    Create addIssue.sh

---
+++ addIssue.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+issuetype_id="10001"
+project_key="TEST"
+description="This is a desction of the issue"
+summary="Test issue created from a shell script"
+
+template='{
+  "fields": {
+    "summary": "%s",
+    "issuetype": {
+      "id": "%s"
+    },
+    "project": {
+      "key": "%s"
+    },
+    
+    "description": {
+      "type": "doc",
+      "version": 1,
+      "content": [
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "text": "%s",
+              "type": "text"
+            }
+          ]
+        }
+      ]
+    }
+  }
+}'
+
+json_final=$(printf "$template" \
+		    "$summary" \
+		    "$issuetype_id" \
+		    "$project_key"  \
+		    "$description")
+
+curl -i -X POST \
+	  -H "Authorization:Basic c2hhbnRhbnVkMzkwQGdtYWlsLmNvbTo2YUpLV1VLTzN0bkR6SUZKNE5BRDdBNDE=" \
+	  -H "Content-Type:application/json" \
+	  -H "X-Atlassian-Token:no-check" \
+	  "https://shantanu391.atlassian.net/rest/api/3/issue/" \
+	  -d \
+	  "$json_final"
